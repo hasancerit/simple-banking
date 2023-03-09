8acb81c1451146353a90f396e94168724893d7fd
hasancerit <19082015H.c>
2023-03-09 22:02:12 +0300

    refactor | added private constructor to class

--- src/main/java/com/eteration/simplebanking/util/RegexUtil.java
+++ src/main/java/com/eteration/simplebanking/util/RegexUtil.java
@@ -1,7 +1,11 @@
 package com.eteration.simplebanking.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 import java.util.regex.Pattern;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RegexUtil {
     public static boolean matches(String value, String regex) {
         final Pattern pattern = Pattern.compile(regex);
