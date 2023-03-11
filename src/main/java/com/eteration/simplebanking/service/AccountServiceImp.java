6f911fab936c2d5a570299b5bef4c798925ed599
hasancerit <19082015H.c>
2023-03-12 00:36:57 +0300

    refactor | fixed duplcations in AccountServiceImp

--- src/main/java/com/eteration/simplebanking/service/AccountServiceImp.java
+++ src/main/java/com/eteration/simplebanking/service/AccountServiceImp.java
@@ -3,6 +3,7 @@ package com.eteration.simplebanking.service;
 import com.eteration.simplebanking.domain.model.AccountNumber;
 import com.eteration.simplebanking.domain.model.Amount;
 import com.eteration.simplebanking.domain.model.account.BankAccount;
+import com.eteration.simplebanking.domain.model.account.Transaction;
 import com.eteration.simplebanking.domain.model.account.transaction.DepositTransaction;
 import com.eteration.simplebanking.domain.model.account.transaction.WithdrawTransaction;
 import com.eteration.simplebanking.repository.BankAccountRepository;
@@ -22,16 +23,17 @@ public class AccountServiceImp implements AccountService {
 
     @Override
     public String credit(String accountNumber, Double amount) {
-        BankAccount bankAccount = this.getBankAccountOrThrowException(accountNumber);
-        String transactionApprovalCode = bankAccount.post(new DepositTransaction(Amount.of(amount)));
-        bankAccountRepository.update(bankAccount);
-        return transactionApprovalCode;
+        return this.post(accountNumber, new DepositTransaction(Amount.of(amount)));
     }
 
     @Override
     public String debit(String accountNumber, Double amount) {
+        return this.post(accountNumber, new WithdrawTransaction(Amount.of(amount)));
+    }
+
+    private String post(String accountNumber, Transaction transaction) {
         BankAccount bankAccount = this.getBankAccountOrThrowException(accountNumber);
-        String transactionApprovalCode = bankAccount.post(new WithdrawTransaction(Amount.of(amount)));
+        String transactionApprovalCode = bankAccount.post(transaction);
         bankAccountRepository.update(bankAccount);
         return transactionApprovalCode;
     }
